05be029fc364cf28f3f32a729f8b17c9645d5553
jopakka <jopakkamies@gmail.com>
2021-09-30 09:24:32 +0300

    basic search works it search from item name

--- app/src/main/java/fi/joonaun/helsinkitour/network/HelsinkiRepository.kt
+++ app/src/main/java/fi/joonaun/helsinkitour/network/HelsinkiRepository.kt
@@ -1,9 +1,49 @@
 package fi.joonaun.helsinkitour.network
 
-class HelsinkiRepository {
-    private val call = HelsinkiApi.service
+import fi.joonaun.helsinkitour.utils.parseHtml
+import java.util.*
 
-    suspend fun getAllPlaces(language: String) = call.getAllPlaces(language)
-    suspend fun getAllEvents(language: String) = call.getAllEvents(language)
-    suspend fun getAllActivities(language: String) = call.getAllActivities(language)
+object HelsinkiRepository {
+    private val call = HelsinkiApi.service
+    private lateinit var activities: Activities
+    private lateinit var events: Events
+    private lateinit var places: Places
+    private val language: String
+
+    init {
+        language = if (Locale.getDefault().language == "fi") "fi" else "en"
+    }
+
+    suspend fun getAllPlaces(): Places {
+        if (!this::places.isInitialized) {
+            places = call.getAllPlaces(language)
+            places.data.forEach {
+                it.description.body = parseHtml(it.description.body)
+            }
+        }
+
+        return places
+    }
+
+    suspend fun getAllEvents(): Events {
+        if (!this::events.isInitialized) {
+            events = call.getAllEvents(language)
+            events.data.forEach {
+                it.description.body = parseHtml(it.description.body)
+            }
+        }
+
+        return events
+    }
+
+    suspend fun getAllActivities(): Activities {
+        if (!this::activities.isInitialized) {
+            activities = call.getAllActivities(language)
+            activities.data.forEach {
+                it.description.body = parseHtml(it.description.body)
+            }
+        }
+
+        return activities
+    }
 }
---
+++ app/src/main/java/fi/joonaun/helsinkitour/utils/Enums.kt
@@ -0,0 +1,5 @@
+package fi.joonaun.helsinkitour.utils
+
+enum class HelsinkiType {
+    ACTIVITY, EVENT, PLACE
+}
