df835845c41cbf2fdaa6ee0f035f8297688e4e12
patep <patrik.polkki@metropolia.fi>
2021-10-06 10:22:30 +0300

    Map coroutine, markerwindow view binding

--- app/src/main/java/fi/joonaun/helsinkitour/ui/map/MapFragment.kt
+++ app/src/main/java/fi/joonaun/helsinkitour/ui/map/MapFragment.kt
@@ -20,6 +20,7 @@ import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceManager
 import com.google.android.material.chip.ChipGroup
 import fi.joonaun.helsinkitour.MainViewModel
@@ -27,6 +28,8 @@ import fi.joonaun.helsinkitour.R
 import fi.joonaun.helsinkitour.databinding.FragmentMapBinding
 import fi.joonaun.helsinkitour.network.Helsinki
 import fi.joonaun.helsinkitour.ui.map.filtersheet.FilterSheet
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -37,7 +40,8 @@ import org.osmdroid.bonuspack.clustering.RadiusMarkerClusterer
 import org.osmdroid.bonuspack.utils.BonusPackHelper
 
 
-class MapFragment : Fragment(R.layout.fragment_map), LocationListener, ChipGroup.OnCheckedChangeListener {
+class MapFragment : Fragment(R.layout.fragment_map), LocationListener,
+    ChipGroup.OnCheckedChangeListener {
     private lateinit var binding: FragmentMapBinding
     lateinit var locationManager: LocationManager
     private val mainViewModel: MainViewModel by activityViewModels()
@@ -101,33 +105,35 @@ class MapFragment : Fragment(R.layout.fragment_map), LocationListener, ChipGroup
     }
 
     private fun addMarker(pointInfo: List<Helsinki>) {
+        lifecycleScope.launch(Dispatchers.IO) {
+            val allMarkers = RadiusMarkerClusterer(requireContext())
 
-        val allMarkers = RadiusMarkerClusterer(requireContext())
-        
-        pointInfo.forEach { point ->
-            try {
-                val myInfoWindow = MyMarkerWindow(binding.map)
-                marker = Marker(binding.map)
-                marker.apply {
-                    icon = AppCompatResources.getDrawable(
-                        requireContext(),
-                        R.drawable.ic_baseline_place_24
-                    )
-                    setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                    position = GeoPoint(point.location.lat, point.location.lon)
-                    infoWindow = myInfoWindow
-                    relatedObject = point
-                    closeInfoWindow()
+            pointInfo.forEach { point ->
+                try {
+                    val myInfoWindow = MyMarkerWindow(binding.map)
+                    marker = Marker(binding.map)
+                    marker.apply {
+                        icon = AppCompatResources.getDrawable(
+                            requireContext(),
+                            R.drawable.ic_baseline_place_24
+                        )
+                        setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                        position = GeoPoint(point.location.lat, point.location.lon)
+                        infoWindow = myInfoWindow
+                        relatedObject = point
+                        closeInfoWindow()
+                    }
+                    allMarkers.add(marker)
+                } catch (e: Exception) {
+                    Log.e("ERROR", "location: ${point.location} error: $e")
                 }
-                allMarkers.add(marker)
-            } catch (e: Exception) {
-                Log.e("ERROR", "location: ${point.location} error: $e")
             }
-        }
-        binding.map.apply {
-            overlays.add(allMarkers)
-            //displays the marker as soon as it has been added.
-            invalidate()
+            binding.map.apply {
+                overlays.add(allMarkers)
+                //displays the marker as soon as it has been added.
+                invalidate()
+            }
+
         }
     }
 
@@ -142,25 +148,25 @@ class MapFragment : Fragment(R.layout.fragment_map), LocationListener, ChipGroup
         mainViewModel.places.removeObserver(helsinkiObserver)
 
         binding.map.overlays.clear()
-        when(checkedId) {
+        when (checkedId) {
             R.id.chip1 -> {
-                    Log.d("CHECKED", binding.chip1.isChecked.toString())
-                    mainViewModel.activities.observe(viewLifecycleOwner, helsinkiObserver)
+                Log.d("CHECKED", binding.chip1.isChecked.toString())
+                mainViewModel.activities.observe(viewLifecycleOwner, helsinkiObserver)
             }
             R.id.chip2 -> {
-                    Log.d("CHECKED", binding.chip2.isChecked.toString())
-                    mainViewModel.events.observe(viewLifecycleOwner, helsinkiObserver)
+                Log.d("CHECKED", binding.chip2.isChecked.toString())
+                mainViewModel.events.observe(viewLifecycleOwner, helsinkiObserver)
             }
             R.id.chip3 -> {
-                    Log.d("CHECKED", binding.chip3.isChecked.toString())
-                    mainViewModel.places.observe(viewLifecycleOwner, helsinkiObserver)
+                Log.d("CHECKED", binding.chip3.isChecked.toString())
+                mainViewModel.places.observe(viewLifecycleOwner, helsinkiObserver)
 
             }
         }
     }
 
     private val fabListener = View.OnClickListener {
-        when(it) {
+        when (it) {
             binding.filterButton -> {
                 val filterSheet = FilterSheet()
 
@@ -168,6 +174,4 @@ class MapFragment : Fragment(R.layout.fragment_map), LocationListener, ChipGroup
             }
         }
     }
-
-
 }
--- app/src/main/java/fi/joonaun/helsinkitour/ui/map/MyMarkerWindow.kt
+++ app/src/main/java/fi/joonaun/helsinkitour/ui/map/MyMarkerWindow.kt
@@ -1,10 +1,16 @@
 package fi.joonaun.helsinkitour.ui.map
 
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.navigation.findNavController
+import fi.joonaun.helsinkitour.BR
 import fi.joonaun.helsinkitour.R
+import fi.joonaun.helsinkitour.databinding.FragmentMapBinding
 import fi.joonaun.helsinkitour.network.Helsinki
 import org.osmdroid.events.MapEventsReceiver
 import org.osmdroid.events.MapListener
@@ -17,16 +23,13 @@ import org.osmdroid.views.overlay.infowindow.InfoWindow
 import org.osmdroid.bonuspack.location.POI
 
 
+class MyMarkerWindow(mapView: MapView) : InfoWindow(R.layout.info_window, mapView) {
 
-
-class MyMarkerWindow(mapView: MapView):  InfoWindow(R.layout.info_window, mapView) {
-
-    private val mapListener = MapEventsOverlay(object  : MapEventsReceiver {
+    private val mapListener = MapEventsOverlay(object : MapEventsReceiver {
         override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
             close()
             return true
         }
-
         override fun longPressHelper(p: GeoPoint?): Boolean {
             Log.d("LONGPRESS", true.toString())
             return true
@@ -41,23 +44,18 @@ class MyMarkerWindow(mapView: MapView):  InfoWindow(R.layout.info_window, mapVie
         val markerInfo = item as Marker
         val selectedMarker = markerInfo.relatedObject as Helsinki
 
+        val viewRoot = LayoutInflater.from(mView.context).inflate(R.layout.info_window, null)
+        val binding: ViewDataBinding? = DataBindingUtil.bind(viewRoot)
 
-        val bubbleTitle = mView.findViewById<TextView>(R.id.bubble_title)
-        val bubbleDes = mView.findViewById<TextView>(R.id.bubble_des)
-        val bubbleAddress = mView.findViewById<TextView>(R.id.bubble_address)
-        val bubbleUrl = mView.findViewById<TextView>(R.id.bubble_url)
+        mView = binding?.root
 
+        binding?.setVariable(BR.helsinkiItem, selectedMarker)
 
+        val bubbleReadMore = mView.findViewById<Button>(R.id.readMoreButton)
 
-        bubbleTitle.text = selectedMarker.name.fi
-        bubbleDes.text = selectedMarker.description.intro ?: selectedMarker.description.body
-        bubbleAddress.text = "${selectedMarker.location.address.streetAddress} ${selectedMarker.location.address.locality}, ${selectedMarker.location.address.postalCode}"
-        bubbleUrl.text = selectedMarker.infoUrl
-        // You can set an onClickListener on the InfoWindow itself.
-        // This is so that you can close the InfoWindow once it has been tapped.
-
-        // Instead, you could also close the InfoWindows when the map is pressed.
-        // This is covered in the Map Listeners guide.
+        bubbleReadMore.setOnClickListener {
+            view.findNavController().navigate()
+        }
 
         mView.setOnClickListener {
             close()
