f0209d969acc730e198732f5b4d58754d61250e7
jopakka <jopakkamies@gmail.com>
2021-10-12 20:16:29 +0300

    map icons better looking small fixes

--- app/src/main/java/fi/joonaun/helsinkitour/ui/map/MapFragment.kt
+++ app/src/main/java/fi/joonaun/helsinkitour/ui/map/MapFragment.kt
@@ -9,7 +9,6 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.os.StrictMode
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -30,8 +29,7 @@ import com.google.android.material.chip.ChipGroup
 import fi.joonaun.helsinkitour.MainViewModel
 import fi.joonaun.helsinkitour.R
 import fi.joonaun.helsinkitour.databinding.FragmentMapBinding
-import fi.joonaun.helsinkitour.network.Helsinki
-import fi.joonaun.helsinkitour.network.HelsinkiRepository
+import fi.joonaun.helsinkitour.network.*
 import fi.joonaun.helsinkitour.utils.getTodayDate
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -94,10 +92,9 @@ class MapFragment : Fragment(R.layout.fragment_map), LocationListener,
             icon = AppCompatResources.getDrawable(
                 requireContext(),
                 R.drawable.ic_baseline_person_pin_circle_24
-            )?.also {
-                it.setTint(Color.DKGRAY)
-            }
+            )
             setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            infoWindow = null
         }
         binding.map.apply {
             overlays.add(userMarker)
@@ -272,9 +269,13 @@ class MapFragment : Fragment(R.layout.fragment_map), LocationListener,
             }
             val allMarkers = RadiusMarkerClusterer(requireContext())
             val drawable =
-                ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_star_24)
-            drawable?.setTint(Color.BLACK)
-            allMarkers.setIcon(drawable?.toBitmap(200, 200))
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_circle_24)
+
+            val density = requireContext().resources.displayMetrics.density
+            val iconSize = (60 * density).roundToInt()
+
+            allMarkers.setIcon(drawable?.toBitmap(iconSize, iconSize))
+            allMarkers.setRadius(iconSize)
 
             val myInfoWindow = MyMarkerWindow(binding.map, this@MapFragment)
             val userLocation: LiveData<Location?> = viewModel.userLocation
@@ -283,11 +284,19 @@ class MapFragment : Fragment(R.layout.fragment_map), LocationListener,
                 try {
                     val marker = Marker(binding.map)
                     marker.apply {
-                        icon = AppCompatResources.getDrawable(
-                            requireContext(),
-                            R.drawable.ic_baseline_place_24
-                        )?.also {
-                            it.setTint(Color.DKGRAY)
+                        icon = when(point) {
+                            is Activity -> AppCompatResources.getDrawable(
+                                requireContext(),
+                                R.drawable.marker_activity
+                            )
+                            is Event -> AppCompatResources.getDrawable(
+                                requireContext(),
+                                R.drawable.marker_event
+                            )
+                            else -> AppCompatResources.getDrawable(
+                                requireContext(),
+                                R.drawable.marker_place
+                            )
                         }
                         setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
                         position = GeoPoint(point.location.lat, point.location.lon)
